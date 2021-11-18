fdfee6ed7db36b3da595f49dee3afcf13f58da93
Rohan Shrivastava <rohansh0808@gmail.com>
2021-11-18 22:36:07 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,16 @@
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter another number: "))
+operator = input('Enter operator: ')
+
+if operator == "+":
+    print(num1+num2)
+elif operator == "-":
+    print(num1-num2)
+elif operator == "*":
+    print(num1*num2)
+elif operator == "/":
+    print(num1/num2)
+elif operator == "%":
+    print(num1%num2)
+elif operator == "**":
+    print(num1**num2)
